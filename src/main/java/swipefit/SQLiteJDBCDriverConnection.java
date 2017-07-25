ee8d27f93f1955923e1534be72d22158e608bd0e
Luta George Gabriel <luta.george.gabriel@gmail.com>
2017-07-26 00:59:12 +0300

    added method that should be called before any before any SQL queries

--- src/main/java/swipefit/SQLiteJDBCDriverConnection.java
+++ src/main/java/swipefit/SQLiteJDBCDriverConnection.java
@@ -8,6 +8,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SQLiteJDBCDriverConnection {
+
+    // method that should be called before any before any SQL queries
+
+    public static Connection connectDb() throws ClassNotFoundException,SQLException {
+        Class.forName("org.sqlite.JDBC");
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:/Users/georgegabriel/Documents/tools/sqlite/database.db");
+        return conn;
+    }
     /**
      * Connect to a sample database
      */
