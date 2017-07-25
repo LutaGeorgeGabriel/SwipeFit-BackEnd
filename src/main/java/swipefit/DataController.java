bf935bb781871c784b1a67e16b6a0aaa436eb39b
Luta George Gabriel <luta.george.gabriel@gmail.com>
2017-07-26 00:56:18 +0300

    added some test methods that allowed me to test the client side of the app, by pushing POST message from the device to the service - they are working and can be as an example for the real ones

--- src/main/java/swipefit/DataController.java
+++ src/main/java/swipefit/DataController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by georgegabriel on 22/07/2017.
@@ -16,6 +17,7 @@ import java.io.IOException;
 public class DataController {
 
     private static double[] info = null;
+    private ArrayList<String> json = new ArrayList<>();
 
     @RequestMapping(value = "/swipeFitProducts", method = RequestMethod.GET)
     public String fetchData() {
@@ -29,9 +31,25 @@ public class DataController {
         return "Error occured";
     }
 
+    // --------------- TEST data
+
     @RequestMapping(value = "/swipeFitProducts",method = RequestMethod.POST)
     public void fetchProductsInformation(@RequestBody double[] productsInformation) {
         info = productsInformation;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/test")
+    public ArrayList<String> getData() {
+        return json;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/test")
+    public void dataFromAndroidTest(@RequestBody String json) {
+        requestHandler(json);
+    }
+
+    public void requestHandler(String json) {
+        this.json.add(json);
     }
 }
 
