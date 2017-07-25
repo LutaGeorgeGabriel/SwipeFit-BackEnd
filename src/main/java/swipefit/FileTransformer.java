005e9310d3abe1f60f08a2c95f40b8d613ba8d04
Luta George Gabriel <luta.george.gabriel@gmail.com>
2017-07-26 00:40:53 +0300

    added new class FileTransformer which will read and write to the file that holds the matrix with likes and dislikes

---
+++ src/main/java/swipefit/FileTransformer.java
@@ -0,0 +1,50 @@
+package swipefit;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.List;
+
+/**
+ * Created by georgegabriel on 25/07/2017.
+ */
+public class FileTransformer {
+
+    static int[][] matrix = new int[200][50];
+
+    // reads the data from input.txt
+
+    public static List<String[]> readMatrix() throws IOException {
+        CSVReader input = new CSVReader(
+                new FileReader("input.txt"), ' ');
+        // Read all rows at once
+
+        List<String[]> allRows = input.readAll();
+
+        return allRows;
+    }
+
+    public static void writeMatrix(List<String[]> data) {
+        StringWriter writer = new StringWriter();
+
+        List<Product> products;
+
+        //List<String[]> data = toStringArray(emps);
+        //TODO cauta in articol: http://www.journaldev.com/12014/opencsv-csvreader-csvwriter-example
+
+        CSVWriter output = new CSVWriter(writer,' ',' ',"\n");
+
+        output.writeAll(data);
+
+        try {
+            output.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(writer);
+    }
+}
