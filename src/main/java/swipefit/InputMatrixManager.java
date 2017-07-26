cb2f68934d784a4ba2f73828b0c69083af41aaa7
Luta George Gabriel <luta.george.gabriel@gmail.com>
2017-07-26 12:42:17 +0300

    added class InputMatrixManager that creates a file with random user behaviour using usersData.R script and after that writes in that text file the data sent from the device(real user) and outputs it as the same file - current bug : file is written only after I stop the program

---
+++ src/main/java/swipefit/InputMatrixManager.java
@@ -0,0 +1,64 @@
+package swipefit;
+
+import com.opencsv.CSVWriter;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by georgegabriel on 26/07/2017.
+ */
+public class InputMatrixManager {
+
+    public static void main(String[] args) {
+        generateOtherUsersBehaviour();
+    }
+
+    public static void generateOtherUsersBehaviour() {
+        Process p;
+        /*try {
+            p = Runtime.getRuntime().exec("R CMD BATCH /Users/georgegabriel/Documents/licenta/SwipeFit-BackEnd/src/main/resources/usersData.R");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        try {
+            Thread.sleep(200);
+            p = Runtime.getRuntime().exec("R CMD BATCH /Users/georgegabriel/Documents/licenta/SwipeFit-BackEnd/src/main/resources/usersData.R");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        List<String[]> users = null;
+
+        try {
+            users = FileTransformer.readMatrix("/Users/georgegabriel/Documents/licenta/SwipeFit-BackEnd/src/main/resources/otherUsersData.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        users.add(DataController.getLikesAndDislikes());
+
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter("/Users/georgegabriel/Documents/licenta/SwipeFit-BackEnd/src/main/resources/otherUsersData.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //using custom delimiter and quote character
+        CSVWriter csvWriter = new CSVWriter(fileWriter,' ', ' ', "\n");
+
+        csvWriter.writeAll(users);
+
+        try {
+            csvWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
