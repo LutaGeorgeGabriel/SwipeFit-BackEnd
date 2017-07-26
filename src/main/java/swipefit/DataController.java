4ebc6a7c16be3331e037e92f7f339ffb56118c2b
Luta George Gabriel <luta.george.gabriel@gmail.com>
2017-07-26 12:38:15 +0300

    added method requestHandler() that transforms the JSON received from device to a HashMap and also added method getLikesAndDislikes() that returns the values from the newly transformed HashMap and retrun it as an array of strings

--- src/main/java/swipefit/DataController.java
+++ src/main/java/swipefit/DataController.java
@@ -1,11 +1,16 @@
 package swipefit;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.lang.reflect.Type;
+import java.util.*;
+
+import static swipefit.InputMatrixManager.generateOtherUsersBehaviour;
 
 /**
  * Created by georgegabriel on 22/07/2017.
@@ -15,7 +20,25 @@ import java.util.ArrayList;
 public class DataController {
 
     private static double[] info = null;
-    private ArrayList<String> json = new ArrayList<>();
+    private static HashMap<String,String> productsInformation = new HashMap<>();
+
+    public static String[] getLikesAndDislikes() {
+
+        List<String> likesAndDislikesList = new ArrayList<>();
+
+        Iterator it = productsInformation.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            System.out.println(pair.getKey() + " = " + pair.getValue());
+            likesAndDislikesList.add(pair.getValue().toString());
+            it.remove(); // avoids a ConcurrentModificationException
+        }
+
+        String[] likesAndDislikesArray = likesAndDislikesList.toArray(new String[10]);
+        return likesAndDislikesArray;
+    }
+
+    // --------------- TEST data
 
     @RequestMapping(value = "/swipeFitProducts", method = RequestMethod.GET)
     public String fetchData() {
@@ -23,25 +46,27 @@ public class DataController {
         return Database2JSON.getJsonFromListOfProducts(Database2JSON.getListOfProducts());
     }
 
-    // --------------- TEST data
-
     @RequestMapping(value = "/swipeFitProducts",method = RequestMethod.POST)
     public void fetchProductsInformation(@RequestBody double[] productsInformation) {
         info = productsInformation;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/test")
-    public ArrayList<String> getData() {
-        return json;
+    public HashMap<String,String> getData() {
+        return productsInformation;
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/test")
     public void dataFromAndroidTest(@RequestBody String json) {
         requestHandler(json);
+        generateOtherUsersBehaviour();
     }
 
     public void requestHandler(String json) {
-        this.json.add(json);
+        Type type = new TypeToken<HashMap<String, String>>(){}.getType();
+        Gson gson = new Gson();
+        Map<String, String> myMap = gson.fromJson(json, type);
+        productsInformation = gson.fromJson(json,type);
     }
 }
 
