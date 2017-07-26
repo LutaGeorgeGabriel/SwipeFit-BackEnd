1f56cea7301f5b0859331ce79377f235b79a5773
Luta George Gabriel <luta.george.gabriel@gmail.com>
2017-07-26 03:05:01 +0300

    modified the endpoint so that now the application withdraws data directly from the database

--- src/main/java/swipefit/DataController.java
+++ src/main/java/swipefit/DataController.java
@@ -1,12 +1,10 @@
 package swipefit;
 
-import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -21,14 +19,8 @@ public class DataController {
 
     @RequestMapping(value = "/swipeFitProducts", method = RequestMethod.GET)
     public String fetchData() {
-        try {
-            return new Data().getData("data.json");
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return "Error occured";
+        //return new Data().getData("data.json");
+        return Database2JSON.getJsonFromListOfProducts(Database2JSON.getListOfProducts());
     }
 
     // --------------- TEST data
