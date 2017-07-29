e1b758f666d8feaf3c89651501eb7f32e6d3229d
Luta George Gabriel <luta.george.gabriel@gmail.com>
2017-07-29 20:12:12 +0300

    created new class JSON2Database that will have static methods to write to database. One of the newly added one is pushFavorites2Database() which will take a list of products that the user marked as favorites and test to see if the products are already in the favorites table; if not, the method pushes it into database using INSERT statement

---
+++ src/main/java/swipefit/JSON2Database.java
@@ -0,0 +1,82 @@
+package swipefit;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by georgegabriel on 29/07/2017.
+ */
+public class JSON2Database {
+
+    public static void pushFavorites2Database(List<Product> favorites) {
+        try {
+            connectDb();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        Connection connection = null;
+        Statement statement = null;
+        // db parameters
+        String url = "jdbc:sqlite:/Users/georgegabriel/Documents/licenta/SwipeFit-BackEnd/swipefit-database.db";
+        // create a connection to the database
+
+        try {
+            connection = DriverManager.getConnection(url);
+            statement = connection.createStatement();
+
+
+            String query = "SELECT id, url, name, site, retailer, price  FROM FAVORITES";
+            ResultSet result = statement.executeQuery(query);
+            List<Product> existingFavorites = new ArrayList<>();
+
+            while (result.next()) {
+                existingFavorites.add(new Product(result.getString("url"),
+                        result.getString("name"),
+                        result.getString("site"),
+                        result.getString("retailer"),
+                        result.getDouble("price"),
+                        result.getInt("id")));
+            }
+
+            if(existingFavorites.isEmpty()) {
+                for(Product favorite : favorites) {
+                    String sql = "INSERT INTO FAVORITES (id, url, name, site, retailer, price) VALUES " +
+                            "( " +
+                            favorite.getID() + "," + "\"" +
+                            favorite.getUrl() + "\"," + "\"" +
+                            favorite.getName() + "\"," + "\"" +
+                            favorite.getSite() + "\"," + "\"" +
+                            favorite.getRetailer() + "\"," +
+                            favorite.getPrice() + ");";
+                    statement.executeUpdate(sql);
+                }
+            } else {
+                for(Product favorite : favorites) {
+                    if(existingFavorites.contains(favorite))
+                        continue;
+                    String sql = "INSERT INTO FAVORITES (id, url, name, site, retailer, price) VALUES " +
+                            "( " +
+                            favorite.getID() + "," + "\"" +
+                            favorite.getUrl() + "\"," + "\"" +
+                            favorite.getName() + "\"," + "\"" +
+                            favorite.getSite() + "\"," + "\"" +
+                            favorite.getRetailer() + "\"," +
+                            favorite.getPrice() + ");";
+                    statement.executeUpdate(sql);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection connectDb() throws ClassNotFoundException,SQLException {
+        Class.forName("org.sqlite.JDBC");
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:/Users/georgegabriel/Documents/licenta/SwipeFit-BackEnd/swipefit-database.db");
+        return conn;
+    }
+}
